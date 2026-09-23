dfc7a0f95c4cdf6bc3cbcc36024b7a27ab19516d
Harshini <harshinij241@gmail.com>
2026-09-23 16:39:13 +0530

    Fix expected result in multiply test
    Corrected the expected result of the multiply test from 15 to 16.

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ from app import multiply, divide
 import pytest
 
 def test_multiply():
-    assert multiply(5,3)==15
+    assert multiply(5,3)==16
 
 def test_divide():
     assert divide(4,2)==2   
